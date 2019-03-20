da961b04ec4efb659ed59a471806497b2ce31d9d
Valeria <vgrazios@nd.edu>
2019-03-20 03:46:18 -0400

    dg

---
+++ Project 2/js/popup-template.js
@@ -0,0 +1,64 @@
+//************************ Quetzal Button ********************
+
+$(document).ready(function (){
+    $('#quetzal-button').click(function() {
+        $("#quetzal-modal").toggleClass("visible");
+        $(".modal").toggleClass("visible");
+        });
+});
+
+$(document).ready(function (){
+    $('#quetzal-close').click(function() {
+        $("#quetzal-modal").toggleClass("visible")
+        $(".modal").toggleClass("visible");
+        });
+});
+
+//************************ Sloth Button *****************
+
+$(document).ready(function (){
+    $('#sloth-button').click(function() {
+        $("#sloth-modal").toggleClass("visible");
+        $(".modal").toggleClass("visible");
+        });
+});
+
+$(document).ready(function (){
+    $('#sloth-close').click(function() {
+        $("#sloth-modal").toggleClass("visible")
+        $(".modal").toggleClass("visible");
+        });
+});
+
+
+//************************ Puma Button *****************
+
+$(document).ready(function (){
+    $('#boa-button').click(function() {
+        $("#boa-modal").toggleClass("visible");
+        $(".modal").toggleClass("visible");
+        });
+});
+
+$(document).ready(function (){
+    $('#boa-close').click(function() {
+        $("#boa-modal").toggleClass("visible")
+        $(".modal").toggleClass("visible");
+        });
+});
+
+//************************ Jaguar Button *****************
+
+$(document).ready(function (){
+    $('#jaguar-button').click(function() {
+        $("#jaguar-modal").toggleClass("visible");
+        $(".modal").toggleClass("visible");
+        });
+});
+
+$(document).ready(function (){
+    $('#jaguar-close').click(function() {
+        $("#jaguar-modal").toggleClass("visible")
+        $(".modal").toggleClass("visible");
+        });
+});
